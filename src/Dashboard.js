dfffc21fb431d10cde05865821c1480b863590ce
Kamal Arieff <kamal.arieff@gmail.com>
2019-11-03 03:29:27 +0800

    Changed Box to Button for user in dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,14 +3,17 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Icon from "@mdi/react";
 import { mdiAccountCircle } from "@mdi/js";
 
 const User = () => {
   return (
-    <Box border={1} m={2}>
-      <Link to="/detail">XXXXXXX</Link>
+    <Box m={2}>
+      <Button fullWidth variant="outlined" component={Link} to={"/detail"}>
+        XXXXXXX
+      </Button>
     </Box>
   );
 };
