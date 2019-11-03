d03f4e5c37664edc5287bfca20308d7300199583
Kamal Arieff <kamal.arieff@gmail.com>
2019-11-03 15:46:40 +0800

    Pass users to the dashboard to enable live and history dashboard

--- src/App.js
+++ src/App.js
@@ -5,12 +5,45 @@ import Dashboard from "./Dashboard";
 import UserDetail from "./UserDetail";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+const users = [
+  {
+    type: "good",
+    users: [
+      {
+        username: "XXXXXXX1"
+      }
+    ]
+  },
+  {
+    type: "suspicious",
+    users: [
+      {
+        username: "XXXXXXX2"
+      }
+    ]
+  },
+  {
+    type: "bad",
+    users: [
+      {
+        username: "XXXXXXX3"
+      }
+    ]
+  }
+];
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
           <Dashboard />
+        </Route>
+        <Route path="/dashboard/live">
+          <Dashboard />
+        </Route>
+        <Route path="/dashboard/history">
+          <Dashboard users={users} />
         </Route>
         <Route path="/detail">
           <UserDetail />
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,21 +4,71 @@ import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import { Link } from "react-router-dom";
 import Icon from "@mdi/react";
 import { mdiAccountCircle } from "@mdi/js";
+import * as R from "ramda";
 
-const User = () => {
+const users = [
+  {
+    type: "good",
+    users: [
+      {
+        username: "XXXXXXX1"
+      }
+    ]
+  },
+  {
+    type: "suspicious",
+    users: [
+      {
+        username: "XXXXXXX2"
+      }
+    ]
+  },
+  {
+    type: "bad",
+    users: [
+      {
+        username: "XXXXXXX3"
+      }
+    ]
+  }
+];
+
+const PillSwitcher = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Tabs
+      value={value}
+      onChange={handleChange}
+      indicatorColor="primary"
+      textColor="primary"
+      aria-label="disabled tabs example"
+    >
+      <Tab label="Live" component={Link} to="/dashboard/live" />
+      <Tab label="History" component={Link} to="/dashboard/history" />
+    </Tabs>
+  );
+};
+
+const User = ({ username }) => {
   return (
     <Box m={2}>
       <Button fullWidth variant="outlined" component={Link} to="/detail">
-        XXXXXXX
+        {username}
       </Button>
     </Box>
   );
 };
 
-const List = ({ title, color }) => {
+const List = ({ title, color, users }) => {
   return (
     <Grid item xs={4}>
       <Box border={1} m={2}>
@@ -28,20 +78,37 @@ const List = ({ title, color }) => {
           </Box>
           <Typography variant="body1">{title}</Typography>
         </Box>
-        <User />
+        {users &&
+          users.map(({ username }) => {
+            return <User username={username} />;
+          })}
       </Box>
     </Grid>
   );
 };
 
-const Dashboard = () => {
+const Dashboard = ({ users = [] }) => {
+  const getUsers = R.curry(type =>
+    R.pipe(
+      R.find(R.propEq("type", type)),
+      R.prop("users")
+    )
+  );
+
+  const goodUsers = getUsers("good")(users);
+  const suspiciousUsers = getUsers("suspicious")(users);
+  const badUsers = getUsers("bad")(users);
+
   return (
     <Container maxWidth="lg">
+      <Box display="flex" flexDirection="row-reverse">
+        <PillSwitcher />
+      </Box>
       <Box border={1} m={2}>
         <Grid container spacing={3}>
-          <List title="Good" color="green" />
-          <List title="Suspicious" color="yellow" />
-          <List title="Bad" color="red" />
+          <List title="Good" color="green" users={goodUsers} />
+          <List title="Suspicious" color="yellow" users={suspiciousUsers} />
+          <List title="Bad" color="red" users={badUsers} />
         </Grid>
       </Box>
     </Container>
